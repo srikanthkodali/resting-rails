76c3db6cbf73ec39ab88adbac295fb4c52e87483
Srikanth Kodali <kodali.4u@gmail.com>
2013-05-11 21:39:17 -0500

    Rails structure

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RestDemo::Application.config.secret_token = 'dbcedda45e130b21510781bd32a6151f4ea213d914cf87760b19bb452a8b50d888d73a8e0049d482ae6a790c803d3a622df8489eab7114375bde786f24347687'
